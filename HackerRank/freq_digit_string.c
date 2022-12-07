4f7fdec88389f20b6b2a54f4622e8b4afbdbf809
Shravan <rginslemearanddgoaprojectstudent3.in@siemens.com>
2022-12-07 17:49:31 +0530

    office update

--- HackerRank/freq_digit_string.c
+++ HackerRank/freq_digit_string.c
@@ -9,22 +9,22 @@ int main()
     char str[1001];
     int len,i=0,j,count=0;
 
-    scanf("%s",str);
-
-   for(i=0;i<10;i++)
+    scanf("%s",str);                              //  |INPUT:| lw4n88j12n1
+   // printf("%d\n",strlen(str));
+   for(i=0;i<10;i++)                              // we need count of 0-9 digits 
    {    
         j=0,count=0;
-        while(str[j] != '\0')
+        while(str[j] != '\0')                     
         {
-            if((str[j]-'0')==i)
+            if((str[j]-'0')==i)                    // every digit(0-9) ASCII(48-57) checked across input string
             {
                 count++;
             }
             j++;
         }
 
-        printf("%d ",count);
-   }
+        printf("%d ",count);                       // print count of every digit 
+   }                                                                             // |OUTPUT:|  0 2 1 0 1 0 0 0 2 0 
 
     return 0;
 }
